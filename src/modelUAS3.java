1df212e4cab6eae16c2e9ce68166020c0776f961
fathurrizqiz <ezzdarkrap@gmail.com>
2023-07-14 18:10:59 +0700

    first commit

---
+++ src/modelUAS3.java
@@ -0,0 +1,84 @@
+public class modelUAS3 {
+    private String id;
+    private String Tittle;
+    private int price;
+    private String Description;
+    private int Stok;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return Tittle;
+    }
+
+    public void setTittle(String tittle) {
+        Tittle = tittle;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public int getStok() {
+        return Stok;
+    }
+
+    public void setStok(int stok) {
+        Stok = stok;
+    }
+
+    public int getRatting() {
+        return Ratting;
+    }
+
+    public void setRatting(int ratting) {
+        Ratting = ratting;
+    }
+
+    public String getBrand() {
+        return Brand;
+    }
+
+    public void setBrand(String brand) {
+        Brand = brand;
+    }
+
+    public String getThunbail() {
+        return Thunbail;
+    }
+
+    public void setThunbail(String thunbail) {
+        Thunbail = thunbail;
+    }
+
+    public String getKategori() {
+        return Kategori;
+    }
+
+    public void setKategori(String kategori) {
+        Kategori = kategori;
+    }
+
+    private int Ratting;
+    private String Brand;
+    private String Thunbail;
+    private String Kategori;
+}
